6c04c64aa0cc0ac333015a234009bb4fc7a25a12
paldepind <simonfv@gmail.com>
2016-07-27 21:42:14 +0200

    Make sure identity is circulair

--- src/monoids/sum.ts
+++ src/monoids/sum.ts
@@ -3,7 +3,7 @@ import {Monoid, MonoidConstructor} from "../monoid";
 export type Sum = ImplSum;
 
 class ImplSum implements Monoid<Sum> {
-  constructor(private n: number) {};
+  constructor(public n: number) {};
   identity = sumId;
   merge(s: Sum): Sum {
     return new ImplSum(this.n + s.n);
@@ -11,11 +11,7 @@ class ImplSum implements Monoid<Sum> {
 }
 
 export const sumId = new ImplSum(0);
-
-interface F { (n: number): any; identity: Monoid<Sum>; }
-
-var f = <F>function (n: number) { return new ImplSum(n); }
-f.identity = sumId;
+sumId.identity = sumId;
 
 interface SumConstructor {
   (a: number): any;
@@ -25,5 +21,9 @@ var temp = <SumConstructor>function(n: number) {
   return new ImplSum(n);
 };
 temp.identity = sumId;
+
+export function toNumber(s: Sum): number {
+  return s.n;
+}
 
 export const Sum: MonoidConstructor<number, Sum> = temp;
