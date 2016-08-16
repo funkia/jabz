998682ddef0116132531eaa39ba59e2c5dc35be8
paldepind <simonfv@gmail.com>
2016-08-16 12:54:06 +0200

    Export everything from index

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,23 @@
+import * as Applicative from "./applicative";
+import * as Either from "./either";
+import * as Foldable from "./foldable";
 import * as Functor from "./functor";
+import * as IO from "./io";
+import * as Maybe from "./maybe";
 import * as Monad from "./monad";
+import * as Monoid from "./monoid";
+import * as Semigroup from "./semigroup";
+import * as Writer from "./writer";
 
 export {
+  Applicative,
+  Either,
+  Foldable,
   Functor,
-  Monad
+  IO,
+  Maybe,
+  Monad,
+  Monoid,
+  Semigroup,
+  Writer
 };
