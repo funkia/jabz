0a967629be411544aaf71061276f94d801459f70
paldepind <simonfv@gmail.com>
2016-10-19 21:09:38 +0200

    Native array works as traversable

--- src/traversable.ts
+++ src/traversable.ts
@@ -13,10 +13,56 @@ export interface Traversable<A> extends Functor<A>, Foldable<A> {
   ): Applicative<Traversable<A>>;
 }
 
+function push<A>(a: A, as: A[]): A[] {
+  as.push(a);
+  return as;
+}
+
+function arraySequence<A>(
+  a: ApplicativeDictionary,
+  t: Applicative<A>[]
+): Applicative<A[]> {
+  let result = a.of<A[]>([]);
+  const lift = result.lift;
+  for (const elm of t) {
+    result = lift(push, elm, result)
+  }
+  return result;
+}
+
+function arrayTraverse<A, B>(
+  a: ApplicativeDictionary,
+  f: (a: A) => Applicative<B>,
+  t: A[]
+): Applicative<B[]> {
+  let result = a.of<B[]>([]);
+  const lift = result.lift;
+  for (const elm of t) {
+    result = lift(push, f(elm), result)
+  }
+  return result;
+}
+
+export function sequence<A>(
+  a: ApplicativeDictionary,
+  t: Traversable<Applicative<A>> | Applicative<A>[]
+): Applicative<Traversable<A>> | Applicative<A[]> {
+  if (t instanceof Array) {
+    return arraySequence(a, t);
+  } else {
+    return t.sequence(a, t);
+  }
+}
+
+// Fixme: Use overload instead
 export function traverse<A, B>(
   a: ApplicativeDictionary,
   f: (a: A) => Applicative<B>,
-  t: Traversable<A>
-): Applicative<Traversable<B>> {
-  return t.traverse(a, f);
+  t: Traversable<A> | A[]
+): Applicative<Traversable<B> | B[]> {
+  if (t instanceof Array) {
+    return arrayTraverse(a, f, t);
+  } else {
+    return t.traverse(a, f);
+  }
 }
--- test/array.ts
+++ test/array.ts
@@ -2,7 +2,9 @@ import "mocha";
 import {assert} from "chai";
 
 import Sum from "../src/monoids/sum";
+import {Maybe, just, nothing} from "../src/maybe";
 import {size, fold, foldMap} from "../src/foldable";
+import {traverse, sequence} from "../src/traversable";
 
 describe("Native list", () => {
   describe("foldable", () => {
@@ -17,6 +19,32 @@ describe("Native list", () => {
     });
     it("size", () => {
       assert.strictEqual(size([1, 2, 3, 4]), 4);
+    });
+  });
+  describe("traversable", () => {
+    it("sequence", () => {
+      assert.deepEqual(
+        sequence(Maybe, [just(1), just(2), just(3)]),
+        just([1, 2, 3])
+      );
+      assert.deepEqual(
+        sequence(Maybe, [just(1), nothing(), just(3)]),
+        nothing()
+      );
+    });
+    it("traverse", () => {
+      function safeParseInt(s: string): Maybe<number> {
+        const n = parseInt(s, 10);
+        return isNaN(n) === true ? nothing() : just(n);
+      }
+      assert.deepEqual(
+        just([1, 2, 3]),
+        traverse(Maybe, safeParseInt, ["1", "2", "3"])
+      );
+      assert.deepEqual(
+        nothing(),
+        traverse(Maybe, safeParseInt, ["1", "two", "3"])
+      );
     });
   });
 });
