e17dc51b69692a1bca5b00c7bb58bebb46f8c7b7
paldepind <simonfv@gmail.com>
2016-12-14 20:05:15 +0100

    Fix syntax highlighting

--- metalsmith.js
+++ metalsmith.js
@@ -1,9 +1,7 @@
 const Metalsmith = require('metalsmith');
-const browserSync = require('metalsmith-browser-sync');
 const watch = require('metalsmith-watch');
 const pug = require('metalsmith-pug/lib/node6');
 const sass = require('metalsmith-sass');
-const highlight = require('metalsmith-code-highlight');
 const metalsmithPrism = require('metalsmith-prism');
 const markdown = require("jstransformer-markdown-it");
 
@@ -11,7 +9,6 @@ Metalsmith(__dirname)
   .source('./docs')
   .destination('./docs-build')
   .use(pug({pretty: true, filters: {markdown}}))
-  // .use(highlight({}))
   .use(metalsmithPrism())
   .use(sass({
     outputStyle: "expanded"
@@ -23,10 +20,6 @@ Metalsmith(__dirname)
     },
     livereload: true
   }))
-  // .use(browserSync({
-  //   server : "./docs-build",
-  //   files  : ["**/*.html", "**/*.css"]
-  // }))
   .build(function(err) {
     if (err) throw err;
   });
