e4f6ed5962fb2b116d3277aff3fb9661e69fbc0f
paldepind <simonfv@gmail.com>
2016-12-17 17:56:01 +0100

    Clean up `combine` type

--- src/semigroup.ts
+++ src/semigroup.ts
@@ -4,11 +4,8 @@ export interface Semigroup<T> {
 
 export type AnySemigroup<A> = string | Array<any> | Semigroup<A>;
 
-export function combine<A>(a: A[], b: A[]): A[];
-export function combine(a: string, b: string): string;
-export function combine<A extends Semigroup<A>>(a: A, b: A): A;
 export function combine<A extends AnySemigroup<A>>(a: A, b: A): A;
-export function combine<A extends Semigroup<A>>(a: any, b: any): A | string | A[] {
+export function combine<A extends AnySemigroup<A>>(a: any, b: any): AnySemigroup<A> {
   if (Array.isArray(a)) {
     return a.concat(b);
   } else if (typeof a === "string") {
