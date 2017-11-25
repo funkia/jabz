3b7f09109ddbb244611831002041b0ca35ee036d
Simon Friis Vindum <simonfv@gmail.com>
2017-11-25 12:17:09 +0100

    Fix problem with circulair dependencies causing a problem in Webpack

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,11 @@ export * from "./monoid";
 export * from "./functor";
 export * from "./applicative";
 export * from "./monad";
+// Note: Maybe must be exported before foldable so that circular
+// dependencies between foldable and maybe are resolved correctly
+export * from "./maybe";
 export * from "./foldable";
 export * from "./traversable";
-
-export * from "./maybe";
 export * from "./either";
 export * from "./conslist";
 export * from "./infinitelist";
